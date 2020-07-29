5eebcffe0a94897155b42dee32750480ae049eaf
Bora Isildak <bora.isildak@cern.ch>
2020-07-29 15:45:34 +0300

    Update build.sh

--- Analysis/build.sh
+++ Analysis/build.sh
@@ -1,6 +1,8 @@
-#!usr/bin/bash
+#!/bin/bash
+
+EXE_NAME=$1
 
 COMPILER=$(root-config --cxx)
 FLAGS=$(root-config --cflags --libs)
 
-$COMPILER -g -O3 -Wall -Wextra -Wpedantic -o SMCEDM_Analysis SMCEDM_Analysis.cxx $FLAGS -lTreePlayer
+$COMPILER -g -O3 -Wall -Wextra -Wpedantic -o $EXE_NAME $EXE_NAME.cxx $FLAGS -lTreePlayer
